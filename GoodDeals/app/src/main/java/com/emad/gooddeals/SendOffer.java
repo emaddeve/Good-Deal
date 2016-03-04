4bae52dd903e0feaec0577cdbdf06682e2e013f1
emaddeve <rifai.deve@gmail.com>
2016-03-04 21:15:16 +0100

    add http post method

---
+++ GoodDeals/app/src/main/java/com/emad/gooddeals/SendOffer.java
@@ -0,0 +1,83 @@
+package com.emad.gooddeals;
+
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+
+/**
+ * this class dealing with the http protocol to send the offer representing in jsonObject to the server
+ * which will be running the background by extending AsyncTask
+ * and reseiving an response code
+ *
+ *
+ * Created by emad on 04/03/16.
+ */
+public class SendOffer extends AsyncTask<Void, Void, Void> {
+    // json object to be send to the server
+    JSONObject parameters;
+    // string to save the response code coming from the server
+    String serverResponse;
+
+    //constructor to initialize the paramerters
+    public SendOffer(JSONObject parameters) {
+        this.parameters = parameters;
+    }
+
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        StringBuffer response = null;
+        try {
+            URL url = new URL("myurl");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+            writer.write(parameters.toString());
+            System.out.println("params.tostring" + parameters.toString());
+
+            writer.close();
+            out.close();
+
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println("Response in universal: " + response.toString());
+        } catch (Exception exception) {
+            System.out.println("Exception: " + exception);
+
+
+        }
+        return null;
+    }
+}
--- GoodDeals/app/src/main/java/com/emad/gooddeals/TakePhoto.java
+++ GoodDeals/app/src/main/java/com/emad/gooddeals/TakePhoto.java
@@ -8,33 +8,42 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 
 
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by emad on 18/02/16.
+ *
+ * Activity for setting up the Offer information
  */
 public class TakePhoto extends Activity {
-
+    ImageToJson imageToJson;
     private ImageView imageView;
-    TextView e;
-    TextView e2;
+
     String encodedImage;
     Bitmap image;
     Double latitude;
     Double longitude;
-    String path;
+
+    EditText offerName;
+    EditText offerDisc;
+
     GPSTracker gps;
+    JSONObject jsonObject;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         imageView = (ImageView) findViewById(R.id.imageView);
-        e = (TextView) findViewById(R.id.textView3);
-        e2 = (TextView) findViewById(R.id.textView4);
+        offerName = (EditText)findViewById(R.id.OfferName);
+        offerDisc = (EditText)findViewById(R.id.OfferDisc);
         getLocation();
+
 
     }
 
@@ -53,28 +62,31 @@ public class TakePhoto extends Activity {
     public void setField() {
         Bundle extras = getIntent().getExtras();
         if (extras == null) {
-
             image = null;
-            path = null;
         } else {
-            latitude = extras.getDouble("latitude");
-            longitude = extras.getDouble("longitude");
-
             image = (Bitmap) extras.get("image");
-            e.setText(latitude.toString());
-            imageView.setImageBitmap(image);
-            e2.setText(encodedImage);
-            // image=loadImageFromStorage(path);
-
-
             imageView.setImageBitmap(image);
 
         }
 
-
-
     }
 
+    public void sendOffer(){
+        try {
+            encodedImage = imageToJson.getStringFromBitmap(image);
+            jsonObject.put("name",offerName.getText().toString());
+            jsonObject.put("Discription",offerDisc.getText().toString());
+            jsonObject.put("longitude",longitude);
+            jsonObject.put("latitude",latitude);
+            jsonObject.put("image",encodedImage);
+            SendOffer asyncT = new SendOffer(jsonObject);
+            asyncT.execute();
 
+
+        } catch (JSONException e1) {
+            e1.printStackTrace();
+        }
+
+    }
 }
 
